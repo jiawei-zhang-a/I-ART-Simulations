e908dcc28ce2f8baef9face0727da03f8a261240
jiawei-zhang-a <jz4721@nyu.edu>
2023-04-02 18:23:11 -0400

    delete uncessary library import

--- Post-Prediction-Imputation/Sbatch/Power.s
+++ Post-Prediction-Imputation/Sbatch/Power.s
@@ -2,16 +2,17 @@
 #
 #SBATCH --job-name=Power
 #SBATCH --nodes=1 
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=1
 #SBATCH --time=5:00
 #SBATCH --mem=12GB
 #SBATCH --cpus-per-task=12
-#SBATCH --output=Power_%A_%a.out
-#SBATCH --error=Power_%A_%a.err
+#SBATCH --output=./Result/Power_%A_%a.out
+#SBATCH --error=./Result/Power_%A_%a.err
 
 
 module purge
-module load python/intel/3.8.6
+mkdir Result
+
 cd ../../
 source venv/bin/activate
 export PATH=/scratch/jz4721/Post-prediction-Causal-Inference/venv/lib64/python3.9/bin:$PATH

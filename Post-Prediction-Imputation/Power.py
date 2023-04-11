9ee869f7cb976bcf04e2b477d22f11a70d25f470
jiawei-zhang-a <jz4721@nyu.edu>
2023-04-10 20:46:42 -0400

    Power: Structure changed to the same as Level

--- Post-Prediction-Imputation/Power.py
+++ Post-Prediction-Imputation/Power.py
@@ -37,57 +37,35 @@ if __name__ == '__main__':
     # Create an instance of the OneShot class
     Framework = OneShot.OneShotTest(N = 1000)
 
-    # power initialization
-    power_median = 0
-    power_LR = 0
-    power_RandomForest = 0
+    print("Begin")
 
-    #iteration 
-    iter = 1
+    # Simulate data
+    DataGen = Generator.DataGenerator(N = 1000, N_T = 500, N_S = 50, beta_11 = 0, beta_12 = 0, beta_21 = 0, beta_22 = 0, beta_23 = 0, beta_31 = 0, MaskRate=0.3,Unobserved=0)
 
-    # correlation initialization
-    corr_median = np.zeros(iter)
-    corr_LR = np.zeros(iter)
-    corr_RandomForest = np.zeros(iter)
+    X, Z, U, Y, M, S = DataGen.GenerateData()
 
-    # Fixed X, Z, change beta to make different Y,M
-    for i in range(iter):
-        
-        print("Iteration: ", i)
-        # Simulate data
-        DataGen = Generator.DataGenerator(N = 1000, N_T = 500, N_S = 50, beta_11 = beta_coef, beta_12 = beta_coef, beta_21 = beta_coef, beta_22 = beta_coef, beta_23 = beta_coef, beta_31 = beta_coef, MaskRate=0.3,Unobserved=0)
+    #Median imputer
+    median_imputer_1 = SimpleImputer(missing_values=np.nan, strategy='median')
+    median_imputer_2 = SimpleImputer(missing_values=np.nan, strategy='median')
+    p11, p12, p21, p22, p31, p32, corr1, corr2, reject = Framework.one_shot_test(Z, X, M, Y, G1=median_imputer_1, G2=median_imputer_2,verbose=1)
+    # Append p-values to corresponding lists
+    p_values_median = [ p11, p12, p21, p22, p31, p32, corr1[2], corr2[2],reject ]
 
-        X, Z, U, Y, M, S = DataGen.GenerateData()
+    #LR imputer
+    BayesianRidge_1 = IterativeImputer(estimator = linear_model.BayesianRidge())
+    BayesianRidge_2 = IterativeImputer(estimator = linear_model.BayesianRidge())
+    p11, p12, p21, p22, p31, p32, corr1, corr2, reject = Framework.one_shot_test(Z, X, M, Y, G1=BayesianRidge_1, G2=BayesianRidge_2,verbose=1)
+    # Append p-values to corresponding lists
+    p_values_LR = [ p11, p12, p21, p22, p31, p32, corr1[2], corr2[2],reject ]
 
-        #Median imputer
-        median_imputer_1 = SimpleImputer(missing_values=np.nan, strategy='median')
-        median_imputer_2 = SimpleImputer(missing_values=np.nan, strategy='median')
-        p11, p12, p21, p22, p31, p32, corr1, corr2, reject = Framework.one_shot_test_parallel(Z, X, M, Y, G1=median_imputer_1, G2=median_imputer_2,verbose=0)
-        power_median += reject
-        corr_median[i] = (corr1[2] + corr2[2]) / 2
+    #XGBoost
+    XGBoost_1= IterativeImputer(estimator = xgb.XGBRegressor())
+    XGBoost_2= IterativeImputer(estimator = xgb.XGBRegressor())
+    p11, p12, p21, p22, p31, p32, corr1, corr2, reject = Framework.one_shot_test(Z, X, M, Y, G1=XGBoost_1, G2=XGBoost_2,verbose=1)
+    # Append p-values to corresponding lists
+    p_values_xgboost = [ p11, p12, p21, p22, p31, p32, corr1[2], corr2[2],reject ]
 
-        #LR imputer
-        BayesianRidge_1 = IterativeImputer(estimator = linear_model.BayesianRidge())
-        BayesianRidge_2 = IterativeImputer(estimator = linear_model.BayesianRidge())
-        p11, p12, p21, p22, p31, p32, corr1, corr2, reject = Framework.one_shot_test_parallel(Z, X, M, Y, G1=BayesianRidge_1, G2=median_imputer_2,verbose=0)
-        power_LR += reject
-        corr_LR[i] = (corr1[2] + corr2[2]) / 2
-
-        #XGBoost
-        XGBoost_1= IterativeImputer(estimator = xgb.XGBRegressor())
-        XGBoost_2= IterativeImputer(estimator = xgb.XGBRegressor())
-        p11, p12, p21, p22, p31, p32, corr1, corr2, reject = Framework.one_shot_test(Z, X, M, Y, G1=XGBoost_1, G2=XGBoost_2,verbose=0)
-        power_RandomForest += reject
-        corr_RandomForest[i] = (corr1[2] + corr2[2]) / 2
-    
-    #Write result into the file
-    print("Correlation of Median Imputer: " + str(np.mean(corr_median)))
-    print("Correlation of LR Imputer: " + str(np.mean(corr_LR)))
-    print("Correlation of XGBoost Imputer: " + str(np.mean(corr_RandomForest)))
-    
-    print("Power of Median Imputer: " + str(power_median/iter))
-    print("Power of LR Imputer: " + str(power_LR/iter))
-    print("Power of XGBoost Imputer: " + str(power_RandomForest/iter))
+    print("Finished")
 
     #Save the file in numpy format
     if(save_file):
@@ -96,14 +74,15 @@ if __name__ == '__main__':
             # If the folder does not exist, create it
             os.makedirs("HPC_Power/%d"%beta_coef)
 
-        # Create numpy arrays
-        correlations = np.array([corr_median, corr_LR, corr_RandomForest])
-        powers = np.array([power_median, power_LR, power_RandomForest])
+        # Convert lists to numpy arrays
+        p_values_median = np.array(p_values_median)
+        p_values_LR = np.array(p_values_LR)
+        p_values_xgboost = np.array(p_values_xgboost)
 
         # Save numpy arrays to files
-        np.save('HPC_Power/%d/correlations_%d'%(beta_coef,task_id), correlations)
-        np.save('HPC_Power/%d/powers_%d'%(beta_coef,task_id), powers)        
-    #file.close()
+        np.save('HPC_Power/%d/p_values_median_%d.npy' % (beta_coef, task_id), p_values_median)
+        np.save('HPC_Power/%d/p_values_LR_%d.npy' % (beta_coef,task_id), p_values_LR)
+        np.save('HPC_Power/%d/p_values_xgboost_%d.npy' % (beta_coef,task_id), p_values_xgboost)      
 
 
 

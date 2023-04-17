720d6b1b4109dc8a44bf516964ada27b4833f355
jiawei-zhang-a <jz4721@nyu.edu>
2023-04-16 22:38:48 -0400

    Level.s change

--- Post-Prediction-Imputation/Level.py
+++ Post-Prediction-Imputation/Level.py
@@ -33,21 +33,21 @@ def run(Nsize, Unobserved, Single, filepath ):
     #Median imputer
     median_imputer_1 = SimpleImputer(missing_values=np.nan, strategy='median')
     median_imputer_2 = SimpleImputer(missing_values=np.nan, strategy='median')
-    p11, p12, p21, p22, p31, p32, corr1, corr2, reject = Framework.one_shot_test(Z, X, M, Y, G1=median_imputer_1, G2=median_imputer_2,verbose=1)
+    p11, p12, p21, p22, p31, p32, corr1, corr2, reject = Framework.one_shot_test_parallel(Z, X, M, Y, G1=median_imputer_1, G2=median_imputer_2,verbose=1)
     # Append p-values to corresponding lists
     p_values_median = [ p11, p12, p21, p22, p31, p32, corr1[2], corr2[2],reject ]
 
     #LR imputer
     BayesianRidge_1 = IterativeImputer(estimator = linear_model.BayesianRidge())
     BayesianRidge_2 = IterativeImputer(estimator = linear_model.BayesianRidge())
-    p11, p12, p21, p22, p31, p32, corr1, corr2, reject = Framework.one_shot_test(Z, X, M, Y, G1=BayesianRidge_1, G2=BayesianRidge_2,verbose=1)
+    p11, p12, p21, p22, p31, p32, corr1, corr2, reject = Framework.one_shot_test_parallel(Z, X, M, Y, G1=BayesianRidge_1, G2=BayesianRidge_2,verbose=1)
     # Append p-values to corresponding lists
     p_values_LR = [ p11, p12, p21, p22, p31, p32, corr1[2], corr2[2],reject ]
 
     #XGBoost
     XGBoost_1= IterativeImputer(estimator = xgb.XGBRegressor())
     XGBoost_2= IterativeImputer(estimator = xgb.XGBRegressor())
-    p11, p12, p21, p22, p31, p32, corr1, corr2, reject = Framework.one_shot_test(Z, X, M, Y, G1=XGBoost_1, G2=XGBoost_2,verbose=1)
+    p11, p12, p21, p22, p31, p32, corr1, corr2, reject = Framework.one_shot_test_parallel(Z, X, M, Y, G1=XGBoost_1, G2=XGBoost_2,verbose=1)
     # Append p-values to corresponding lists
     p_values_xgboost = [ p11, p12, p21, p22, p31, p32, corr1[2], corr2[2],reject ]
 

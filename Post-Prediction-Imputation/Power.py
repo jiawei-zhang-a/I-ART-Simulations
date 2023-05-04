cd368cb578d87a111c1a8804bab1699bc0accbe4
jiawei-zhang-a <jz4721@nyu.edu>
2023-05-04 17:17:54 -0400

    MAINT

--- Post-Prediction-Imputation/Power.py
+++ Post-Prediction-Imputation/Power.py
@@ -118,7 +118,7 @@ if __name__ == '__main__':
 
     for coef in np.arange(0.01,0.2,0.01):
         beta_coef = coef
-        run(1000, Unobserved = 0, Single = 1 , filepath = "Result/HPC_power_1000" + "_single")
+        run(1000, Unobserved = 0, Single = 0 , filepath = "Result/HPC_power_1000" + "_single")
         run(1000, Unobserved = 1, Single = 1, filepath = "Result/HPC_power_unobserved_1000" + "_single")
         run(2000, Unobserved = 1, Single = 1, filepath = "Result/HPC_power_unobserved_2000" + "_single")
         run(2000, Unobserved = 0, Single = 1 , filepath = "Result/HPC_power_2000" + "_single")

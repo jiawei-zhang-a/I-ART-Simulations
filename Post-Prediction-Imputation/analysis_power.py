bead3289224758a93435535e86e8d6b5626aec0e
jiawei-zhang-a <jz4721@nyu.edu>
2023-05-17 12:36:42 -0400

    Bug:Fix multi reqires different in analysis

--- Post-Prediction-Imputation/analysis_power.py
+++ Post-Prediction-Imputation/analysis_power.py
@@ -1,56 +1,6 @@
 import numpy as np
 import os
 
-def read_and_print_npz_files(directory, file):
-    
-    file.write("Analysis of : " + directory + "\n")
-
-    summed_p_values_median = None
-    summed_p_values_LR = None
-    summed_p_values_xgboost = None
-    summed_p_values_oracle = None
-
-    N = int(len(os.listdir(directory)) / 4)
-    # Loop through all the files in the directory
-    for filename in os.listdir(directory):
-
-        # Check if the file is a numpy file
-        if filename.endswith(".npy"):
-            # Load the numpy array from the file
-            filepath = os.path.join(directory, filename)
-            p_values = np.load(filepath)
-
-            # Add the array to the running sum based on the filename
-            if "p_values_median" in filename:
-                if summed_p_values_median is None:
-                    summed_p_values_median = p_values
-                else:
-                    summed_p_values_median += p_values
-            elif "p_values_LR" in filename:
-                if summed_p_values_LR is None:
-                    summed_p_values_LR = p_values
-                else:
-                    summed_p_values_LR += p_values
-            elif "p_values_xgboost" in filename:
-                if summed_p_values_xgboost is None:
-                    summed_p_values_xgboost = p_values
-                else:
-                    summed_p_values_xgboost += p_values
-            elif "p_values_oracle" in filename:
-                if summed_p_values_oracle is None:
-                    summed_p_values_oracle = p_values
-                else:
-                    summed_p_values_oracle += p_values
-
-    file.write("Mean p-values for Median Imputer:\n")
-    file.write(str(summed_p_values_median/N) + "\n")
-    file.write("Mean p-values for LR Imputer:\n")
-    file.write(str(summed_p_values_LR/N) + "\n")
-    file.write("Mean p-values for XGBoost Imputer:\n")
-    file.write(str(summed_p_values_xgboost/N) + "\n")
-    file.write("Mean p-values for Oracle:\n")
-    file.write(str(summed_p_values_oracle/N) + "\n")
-
 def read_npz_files(directory):
     summed_p_values_median = None
     summed_p_values_LR = None
@@ -86,30 +36,14 @@ def read_npz_files(directory):
                     summed_p_values_oracle += p_values
 
     results = {
-        'median_power': summed_p_values_median[1] / N,
-        'median_corr': summed_p_values_median[2] / N,
-        'lr_power': summed_p_values_LR[1] / N,
-        'lr_corr': summed_p_values_LR[2] / N,
-        'xgboost_power': summed_p_values_xgboost[1] / N,
-        'xgboost_corr': summed_p_values_xgboost[2] / N,
-        'oracle_power': summed_p_values_oracle[1] / N,
-        'oracle_corr': summed_p_values_oracle[2] / N
+        'median_power': summed_p_values_median[3] / N,
+        'median_corr': summed_p_values_median[4] / N,
+        'lr_power': summed_p_values_LR[3] / N,
+        'lr_corr': summed_p_values_LR[4] / N,
+        'xgboost_power': summed_p_values_xgboost[3] / N,
+        'xgboost_corr': summed_p_values_xgboost[4] / N,
+        'oracle_power': summed_p_values_oracle[3] / N,
+        'oracle_corr': summed_p_values_oracle[4] / N
     }
     return results
-
-def main():
-    for coef in np.arange(0.01, 0.3, 0.03):
-        with open("power.result", "a") as file:
-            file.write("beta: " + str(coef) + "\n")
-            read_and_print_npz_files("Result/HPC_power_unobserved_1000_single/%f" % coef, file)
-            file.write("\n")
-            read_and_print_npz_files("Result/HPC_power_1000_single/%f" % coef, file)
-            file.write("\n")
-            read_and_print_npz_files("Result/HPC_power_unobserved_2000_single/%f" % coef, file)
-            file.write("\n")
-            read_and_print_npz_files("Result/HPC_power_2000_single/%f" % coef, file)
-            file.write("\n")
-
-
-#main()
 

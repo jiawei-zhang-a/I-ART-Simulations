3febd34dfbf374cc0f5a214239246eea4e452ba0
jiawei-zhang-a <jz4721@nyu.edu>
2023-06-20 21:01:21 +0800

    ENH: visulization add new plot based on three sizes

--- Post-Prediction-Imputation/Visulization/plot_size.py
+++ Post-Prediction-Imputation/Visulization/plot_size.py
@@ -2,14 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def plot_results(data):
-    columns = ['beta', 'I50_Median', 'I50_Linear', 'I50_XGBoost', 'I50_Oracle', 'I100_Median', 'I100_Linear', 'I100_XGBoost', 'I100_Oracle']
+    columns = ['beta', 'I50_Median', 'I50_Linear', 'I50_XGBoost', 'I50_Oracle', 
+               'I2000_Median', 'I2000_Linear', 'I2000_XGBoost', 'I2000_Oracle', 
+               'I20000_Median', 'I20000_Linear', 'I20000_XGBoost', 'I20000_Oracle']
 
     df = pd.DataFrame(data, columns=columns)
 
     plt.figure(figsize=(10, 6))
 
     colors = {'Median': 'blue', 'Linear': 'red', 'XGBoost': 'green', 'Oracle': 'purple'}
-    linestyles = {'I50': '-', 'I100': '--'}
+    linestyles = {'I50': '-', 'I2000': '--', 'I20000': ':'}
 
     for col in columns[1:]:
         method = col.split('_')[1]
@@ -25,13 +27,7 @@ def plot_results(data):
 
     plt.show()
 
-# Example usage with a sample dataset
+# You can update your data accordingly
 data = [
-    [4, 0.1385, 0.141, 0.2405, 0.125, 0.197, 0.1915, 0.3455, 0.12],
-    [6, 0.199, 0.287, 0.4295, 0.18, 0.2775, 0.4185, 0.6195, 0.15],
-    [8, 0.2385, 0.441, 0.602, 0.22, 0.3795, 0.705, 0.836, 0.19],
-    [10, 0.316, 0.632, 0.7115, 0.29, 0.5165, 0.8855, 0.926, 0.25],
-    [12, 0.3925, 0.7735, 0.761, 0.35, 0.6345, 0.96, 0.8905, 0.33],
-    [14, 0.5125, 0.8875, 0.8, 0.45, 0.752, 0.989, 0.8775, 0.42],
-    [16, 0.582, 0.9455, 0.84, 0.51, 0.8345, 0.998, 0.911, 0.49]
-]
+    # These are example values, adjust them according to your data
+    [4, 0.1385, 0.141, 0.2405, 0.125, 0.1, 0.2, 0.3, 0.4, 0.
--- Post-Prediction-Imputation/plot_power_comparison.py
+++ Post-Prediction-Imputation/plot_power_comparison.py
@@ -22,34 +22,25 @@ def plot(data_with_U):
     
 def main2(type):
     Power_data = []
-    Power_data_with_LR_adjusted = []
-    Power_data_with_XGBoost_adjusted = []
 
-    for coef in np.arange(0.0,0.5,0.05):
+    for coef in np.arange(0.0,0.2,0.02):
         row_power = [coef]
-        row_power_with_LR_adjusted = [coef]
-        row_power_with_XGBoost_adjusted = [coef]
-        for directory in ["Result/HPC_power_unobserved_1000_single/%f" % (coef)]:
+        for directory in ["Result/HPC_power_50_single/%f" % (coef)]:
             results = read_npz_files(directory)
             row_power.extend([results['median_power'], results['lr_power'], results['xgboost_power'], results['oracle_power']])
 
-        for directory in [ "Result/HPC_power_unobserved_1000_LR_single/%f" % (coef)]:
+        for directory in [ "Result/Result/HPC_power_2000_single/%f" % (coef)]:
             results = read_npz_files(directory)
-            row_power_with_LR_adjusted.extend([results['median_power'], results['lr_power'], results['xgboost_power'], results['oracle_power']])
+            row_power.extend([results['median_power'], results['lr_power'], results['xgboost_power'], results['oracle_power']])
 
-        for directory in [ "Result/HPC_power_unobserved_1000_xgboost_single/%f" % (coef)]:
+        for directory in [ "Result/HPC_power_20000_single/%f" % (coef)]:
             results = read_npz_files(directory)
-            row_power_with_XGBoost_adjusted.extend([results['median_power'], results['lr_power'], results['xgboost_power'], results['oracle_power']])
+            row_power.extend([results['median_power'], results['lr_power'], results['xgboost_power'], results['oracle_power']])
 
         Power_data.append(row_power)
-        Power_data_with_LR_adjusted.append(row_power_with_LR_adjusted)
-        Power_data_with_XGBoost_adjusted.append(row_power_with_XGBoost_adjusted)
     print(Power_data)
-    print(Power_data_with_LR_adjusted)
-    print(Power_data_with_XGBoost_adjusted)
 
-    plot_covariance.plot_results(Power_data, Power_data_with_LR_adjusted, Power_data_with_XGBoost_adjusted)
-    plot_covariance.plot_results2(Power_data, Power_data_with_LR_adjusted, Power_data_with_XGBoost_adjusted)
+    plot_size.plot_results(Power_data)
 
 main2("single")
 

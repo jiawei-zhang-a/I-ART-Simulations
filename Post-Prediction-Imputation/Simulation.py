e4419ae550aa872614baffbabe0eee18fae8b896
jiawei-zhang-a <jz4721@nyu.edu>
2023-07-19 22:14:29 -0400

    MAINT

--- Post-Prediction-Imputation/Simulation.py
+++ Post-Prediction-Imputation/Simulation.py
@@ -95,7 +95,7 @@ class DataGenerator:
 
       for i in range(self.N_S):
           strata = X[i * groupSize : (i+1) * groupSize, 0]  # select the first column in the strata
-          biases.append(np.full(groupSize, 10 * np.mean(strata)))
+          biases.append(np.full(groupSize, np.mean(strata)))
 
       biases = np.concatenate(biases).reshape(-1,)
       return biases

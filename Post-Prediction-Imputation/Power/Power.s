764ed5f8d121284edf904721e7ff8b804ba149be
jiawei-zhang-a <jz4721@nyu.edu>
2023-08-08 14:20:53 -0400

    MAINT

--- Post-Prediction-Imputation/Power/Power.s
+++ Post-Prediction-Imputation/Power/Power.s
@@ -12,7 +12,6 @@ module purge
 
 source /scratch/zc2157/jiawei/Post-prediction-Causal-Inference/venv/bin/activate
 export PATH=/scratch/zc2157/jiawei/Post-prediction-Causal-Inference/venv/lib64/python3.8/bin:$PATH
-source ~/.bashrc
 
 
 cd ../

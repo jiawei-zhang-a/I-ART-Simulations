15654edccf9991123129c4e407511a74e7b42974
jiawei-zhang-a <jz4721@nyu.edu>
2023-09-20 21:23:19 -0400

    label change

--- Post-Prediction-Imputation/plot_power_comparison.py
+++ Post-Prediction-Imputation/plot_power_comparison.py
@@ -45,17 +45,19 @@ def plot_results(data, title, xsticks):
     for col in columns[1:]:
         plt.plot(df['beta'], df[col], marker='o', color=colors[col], linestyle=linestyles[col])
 
-    plt.xlabel(r'$\beta$')
-    plt.ylabel('Power')
+    plt.xlabel(r'$\beta$',fontsize=25)
+    plt.ylabel('Power',fontsize=25)
     plt.grid()
 
     if not os.path.exists("pic"):
         os.makedirs("pic")
 
     plt.xticks(xsticks)
-    y_ticks = [i / 100.0 for i in range(0, 105, 20)]
+    y_ticks = [i / 100.0 for i in range(25, 105, 25)]
     y_ticks.append(0.05)
     plt.yticks(y_ticks)
+    plt.tick_params(axis='both', which='major', labelsize=25)
+
 
     plt.savefig("pic/" + title + ".svg", format='svg')
 
@@ -73,10 +75,10 @@ def main():
 
     for coef in np.arange(0.0,0.3 ,0.05):
         row_power = [coef]
-        for directory in [ "Result/HPC_power_1000_unobserved_interference_single/%f" % (coef)]:
+        for directory in [ "Result_Interference/HPC_power_1000_unobserved_interference_single/%f" % (coef)]:
             results = read_npz_files(directory,small_size=False)
             row_power.extend([ results['lightGBM_power'], results['oracle_power'], results['median_power'], results['lr_power']])
-        for directory in ["Result/HPC_power_1000_unobserved_interference_adjusted_single/%f" % (coef)]:
+        for directory in ["Result_Interference/HPC_power_1000_unobserved_interference_adjusted_single/%f" % (coef)]:
             results = read_npz_files(directory,small_size=False)
             row_power.extend([ results['lightGBM_power'],results['oracle_power'], results['median_power'], results['lr_power'] ])
         Power_data.append(row_power)
@@ -86,10 +88,10 @@ def main():
 
     for coef in np.arange(0.0,1.2,0.2):
         row_power_small = [coef]
-        for directory in ["Result/HPC_power_50_unobserved_interference_single/%f" % (coef)]:
+        for directory in ["Result_Interference/HPC_power_50_unobserved_interference_single/%f" % (coef)]:
             results = read_npz_files(directory,small_size=True)
             row_power_small.extend([results['xgboost_power'], results['oracle_power'], results['median_power'], results['lr_power']])
-        for directory in [ "Result/HPC_power_50_unobserved_interference_adjusted_single/%f" % (coef)]:
+        for directory in [ "Result_Interference/HPC_power_50_unobserved_interference_adjusted_single/%f" % (coef)]:
             results = read_npz_files(directory,small_size=True)
             row_power_small.extend([results['xgboost_power'], results['oracle_power'], results['median_power'], results['lr_power']])
         Power_data_small.append(row_power_small)

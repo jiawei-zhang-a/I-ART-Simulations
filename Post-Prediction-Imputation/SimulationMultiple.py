bed2ea93bfb515f7c09127d1cd6bf864aa5ae234
jiawei-zhang-a <jz4721@nyu.edu>
2024-03-04 23:35:44 -0500

    MAINT

--- Post-Prediction-Imputation/SimulationMultiple.py
+++ Post-Prediction-Imputation/SimulationMultiple.py
@@ -160,7 +160,7 @@ class DataGenerator:
     Y_n1 = 1/4 * self.beta * Z + self.beta * Z * sum1   + sum2 + sum3 + np.sin(U) + StrataEps[:,0]  + IndividualEps[:,0]
 
     # Compute Yn2
-    Y_n2 = self.beta * Z  + self.beta * Z * X[:,0] + sum9 - sum4 + StrataEps[:,1] + IndividualEps[:,1]
+    Y_n2 = self.beta * Z  + self.beta * Z * X[:,0] + sum9 + StrataEps[:,1] + IndividualEps[:,1]
 
     # Compute Yn3
     Y_n3 = self.beta * Z * sum5  + sum6 + sum8 + U +  StrataEps[:,2]  + IndividualEps[:,2]

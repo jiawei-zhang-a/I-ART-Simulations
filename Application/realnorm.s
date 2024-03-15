0ff5941b46668ac7a86b6a9ac3b3d05c68ef1263
jiawei-zhang-a <jz4721@nyu.edu>
2024-03-14 22:50:11 -0400

    MAINT

---
+++ Application/realnorm.s
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH --job-name=Real
+#SBATCH --nodes=1
+#SBATCH --time=21:29:00
+#SBATCH --mem=250
+#SBATCH --cpus-per-task=28
+
+export OMP_NUM_THREADS=1
+
+module purge
+
+source /scratch/jz4721/Post-prediction-Causal-Inference/venv/bin/activate
+export PATH=/scratch/jz4721/Post-prediction-Causal-Inference/venv/lib64/python3.8/bin:$PATH
+
+python MainNormalize.py

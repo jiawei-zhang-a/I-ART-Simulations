851e868099bc8018ca27a3a3adabfd12076e2296
jiawei-zhang-a <jz4721@nyu.edu>
2024-03-19 16:59:34 -0400

    MAINT

--- Application/CIMain.py
+++ Application/CIMain.py
@@ -14,7 +14,7 @@ random_state = 0
 threshholdForX = 0.0
 
 # Define β values range 
-beta_values = np.linspace(-0.5, 0.05, 2000)  
+beta_values = np.linspace(-2, 0.03, 2000)  
 
 # Retrieve the job array index from SLURM
 array_index = int(sys.argv[1]) - 1 

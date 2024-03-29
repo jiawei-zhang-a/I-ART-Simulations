49cf7b36b745f3c959ff69d96db70006cb85a9ac
jiawei-zhang-a <jz4721@nyu.edu>
2024-03-29 19:37:33 -0400

    MAINT

--- Application/CI.s
+++ Application/CI.s
@@ -1,19 +1,15 @@
 #!/bin/bash
 #
-#SBATCH --job-name=Real
+#SBATCH --job-name=CI
 #SBATCH --nodes=1
-#SBATCH --time=35:29:00
+#SBATCH --time=09:29:00
 #SBATCH --mem=250G # Ensure you specify 'G' for GB
-#SBATCH --cpus-per-task=28
+#SBATCH --cpus-per-task=1
 
 export OMP_NUM_THREADS=1
 
 module purge
 
-# No need to activate the previous venv, we will use Singularity
-
-# Adjust the Singularity exec command below as necessary
-# Note: Make sure the paths match where your Singularity image and overlay are located
 singularity exec --nv \
     --overlay /scratch/$USER/Post-prediction-Env/overlay-15GB-500K.ext3:ro \
     /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif \

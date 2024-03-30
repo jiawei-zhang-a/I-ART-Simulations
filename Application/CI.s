21899e45b9c832782993087b1f8fb17ae84cd4f0
jiawei-zhang-a <jz4721@nyu.edu>
2024-03-29 21:29:33 -0400

    MAINT

--- Application/CI.s
+++ Application/CI.s
@@ -11,7 +11,7 @@ export OMP_NUM_THREADS=1
 module purge
 
 singularity exec --nv \
-    --overlay /scratch/$USER/Post-prediction-Env/overlay-15GB-500K.ext3:ro \
+    --overlay /scratch/jz4721/pyenv/overlay-15GB-500K.ext3:ro \
     /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif \
     /bin/bash -c "source /ext3/env.sh; python /scratch/jz4721/Post-prediction-Causal-Inference/Application/CIMain.py $SLURM_ARRAY_TASK_ID"
 
--- Application/real.s
+++ Application/real.s
@@ -10,6 +10,6 @@
 module purge
 
 singularity exec --nv \
-    --overlay /scratch/$USER/Post-prediction-Env/overlay-15GB-500K.ext3:ro \
+    --overlay /scratch/jz4721/pyenv/overlay-15GB-500K.ext3:ro \
     /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif \
     /bin/bash -c "source /ext3/env.sh; python /scratch/jz4721/Post-prediction-Causal-Inference/Application/Main.py"

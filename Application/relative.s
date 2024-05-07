a023b71414e936c320b2710fee3078ecb349d138
jiawei-zhang-a <jz4721@nyu.edu>
2024-05-06 22:01:37 -0400

    MAINT

--- Application/relative.s
+++ Application/relative.s
@@ -12,4 +12,4 @@ module purge
 singularity exec --nv \
     --overlay /scratch/jz4721/pyenv/overlay-15GB-500K.ext3:ro \
     /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif \
-    /bin/bash -c "source /ext3/env.sh; python /scratch/jz4721/Post-prediction-Causal-Inference/Application/MainCombined.py"
+    /bin/bash -c "source /ext3/env.sh; python /scratch/jz4721/Post-prediction-Causal-Inference/Application/MainRelative.py"

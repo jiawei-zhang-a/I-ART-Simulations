49efeb2e1cb63e54990cbbd4d745c0ed90c6a4c3
jiawei-zhang-a <jz4721@nyu.edu>
2024-05-08 20:27:16 -0400

    MAINT

--- Simulation/CalculatePowerForSingleOutcome.py
+++ Simulation/CalculatePowerForSingleOutcome.py
@@ -5,6 +5,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.impute import IterativeImputer
 from sklearn import linear_model
 import SingleOutcomeModelGenerator as Generator
+import MultipleOutcomeModelGenerator as GeneratorMutiple
 import RandomizationTest as RandomizationTest
 import os
 import iArt
@@ -46,19 +47,21 @@ class NoOpImputer(BaseEstimator, TransformerMixin):
         return self.fit(X, y).transform(X)
 
 
-def run(Nsize, filepath, adjust, Missing_lambda, strata_size = 10,model = 0, verbose=1, save_file = True, small_size = True):
+def run(Nsize, filepath,  Missing_lambda,adjust = 0, model = 0, verbose=1, small_size = True, multiple = False):
     
     Missing_lambda = None
 
     if beta_coef == 0:
         Iter = 5000
     else:
-        Iter = L    
-    
+        Iter = L        
     # Simulate data
-    DataGen = Generator.DataGenerator(N = Nsize, strata_size=strata_size,beta = beta_coef,model = model, MaskRate=0.5, verbose=verbose,Missing_lambda = Missing_lambda)
-
-    X, Z, U, Y, M, S = DataGen.GenerateData()
+    if multiple == False:
+        DataGen = Generator.DataGenerator(N = Nsize, strata_size=10,beta = beta_coef,model = model, MaskRate=0.5, verbose=verbose,Missing_lambda = Missing_lambda)
+        X, Z, U, Y, M, S = DataGen.GenerateData()
+    else:
+        DataGen = GeneratorMutiple.DataGenerator(N = Nsize, strata_size=10,beta = beta_coef, MaskRate=0.5, verbose=verbose,Missing_lambda = Missing_lambda)
+        X, Z, U, Y, M, S = DataGen.GenerateData()
 
     #Oracale imputer
     """NoOp = NoOpImputer()
@@ -121,27 +124,25 @@ def run(Nsize, filepath, adjust, Missing_lambda, strata_size = 10,model = 0, ver
     values_complete = [ *p_values, reject ]"""
 
     #Save the file in numpy format
-    if(save_file):
-        if not os.path.exists("%s/%f"%(filepath,beta_coef)):
-            # If the folder does not exist, create it
-            os.makedirs("%s/%f"%(filepath,beta_coef))
+    if not os.path.exists("%s/%f"%(filepath,beta_coef)):
+        # If the folder does not exist, create it
+        os.makedirs("%s/%f"%(filepath,beta_coef))
 
-        # Save numpy arrays to files
-        np.save('%s/%f/p_values_median_%d.npy' % (filepath, beta_coef, task_id), values_median)
-        np.save('%s/%f/p_values_medianadjusted_%d.npy' % (filepath, beta_coef, task_id), values_medianLR)
+    # Save numpy arrays to files
+    np.save('%s/%f/p_values_median_%d.npy' % (filepath, beta_coef, task_id), values_median)
+    np.save('%s/%f/p_values_medianadjusted_%d.npy' % (filepath, beta_coef, task_id), values_medianLR)
 
-        """np.save('%s/%f/p_values_oracle_%d.npy' % (filepath, beta_coef, task_id), values_oracle)
-        np.save('%s/%f/p_values_LR_%d.npy' % (filepath, beta_coef, task_id), values_LR)
-        if small_size == True:
-            np.save('%s/%f/p_values_xgboost_%d.npy' % (filepath, beta_coef, task_id), values_xgboost)
-        if small_size == False:
-            np.save('%s/%f/p_values_lightgbm_%d.npy' % (filepath, beta_coef, task_id), values_lightgbm)
-        np.save('%s/%f/p_values_complete_%d.npy' % (filepath, beta_coef, task_id), values_complete)"""
+    """np.save('%s/%f/p_values_oracle_%d.npy' % (filepath, beta_coef, task_id), values_oracle)
+    np.save('%s/%f/p_values_LR_%d.npy' % (filepath, beta_coef, task_id), values_LR)
+    if small_size == True:
+        np.save('%s/%f/p_values_xgboost_%d.npy' % (filepath, beta_coef, task_id), values_xgboost)
+    if small_size == False:
+        np.save('%s/%f/p_values_lightgbm_%d.npy' % (filepath, beta_coef, task_id), values_lightgbm)
+    np.save('%s/%f/p_values_complete_%d.npy' % (filepath, beta_coef, task_id), values_complete)"""
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         task_id = int(sys.argv[1])
-        save_file = True
     else:
         print("Please add the job number like this\nEx.python Power.py 1")
         exit()
@@ -237,3 +238,53 @@ if __name__ == '__main__':
         else:
             print(f"No lambda value found for beta_coef: {beta_coef_rounded}")
     
+        # Lambda values dictionary
+    lambda_values = {
+        50: {
+            0.0: [5.46301136050662, 1.7687104800990539, 3.6986401066938748],
+            0.12: [5.507071138438006, 1.8832179319883895, 3.8250507348009557],
+            0.24: [5.629938938721568, 1.9080170719416063, 3.870429428753654],
+            0.36: [5.709076777442875, 1.9590050193610664, 4.018691917409632],
+            0.48: [5.831068183224691, 1.9638039860442473, 4.032046646076915],
+            0.6: [5.890152740793354, 2.0340630188325295, 4.188578787477003]
+        },
+        1000: {
+            0.0: [5.445126353777186, 1.7944628138115826, 3.6890049854144222],
+            0.03: [5.448889434968681, 1.799820386146107, 3.69899976186121],
+            0.06: [5.481108645836731, 1.808888277601773, 3.7215141167626897],
+            0.09: [5.518540969761793, 1.8313022068804186, 3.7592034824941227],
+            0.12: [5.509295189307611, 1.824491093343858, 3.7653155995566836],
+            0.15: [5.5323113856789075, 1.829439262086321, 3.7932522695382818]
+        }
+    }
+
+    # 1000 size coef loop
+    for coef in np.arange(0.0, 0.4, 0.05): 
+        beta_coef = coef
+        run(1000, filepath="ResultMultiple/HPC_power_1000_model5",adjust =0,  Missing_lambda=lambda_values[1000].get(coef, None),model = 5, small_size=False, multiple = True)
+    # 50 size coef loop
+    for coef in np.arange(0.0, 2.5, 0.5): 
+        beta_coef = coef
+        run(50, filepath="ResultMultiple/HPC_power_50_model5",adjust =0, Missing_lambda=lambda_values[50].get(coef, None),model = 5, small_size=True, multiple = True)
+    
+    beta_to_lambda = {0.0: 15.52272711345184, 0.1: 15.686703500976, 0.2: 15.686402633876, 0.3: 15.787598335083226, 0.4: 15.753018503387455, 0.5: 15.73965750718643}
+    for coef in np.arange(0.0,0.6 ,0.1):
+        beta_coef = coef
+        # Round to two decimal places to match dictionary keys
+        beta_coef_rounded = round(beta_coef, 2)
+        if beta_coef_rounded in beta_to_lambda:
+            lambda_value = beta_to_lambda[beta_coef_rounded]
+            run(Nsize = 1000, filepath = "Result/HPC_power_1000_model6",adjust =0,  model = 6, Missing_lambda = lambda_value, small_size=False)
+        else:
+            print(f"No lambda value found for beta_coef: {beta_coef_rounded}")
+
+    beta_to_lambda = {0.0: 15.583775008005304, 3.0: 16.2044899667755, 6.0: 16.364986769719895, 9.0: 16.572385216230238, 12.0: 16.508220779651012, 15.0: 16.572190364153975}
+    for coef in np.arange(0.0,18,3):
+        beta_coef = coef
+        # Round to two decimal places to match dictionary keys
+        beta_coef_rounded = round(beta_coef, 2)
+        if beta_coef_rounded in beta_to_lambda:
+            lambda_value = beta_to_lambda[beta_coef_rounded]
+            run(Nsize = 50, filepath = "Result/HPC_power_50_model6",adjust =0,  model = 6, Missing_lambda = lambda_value, small_size=True)
+        else:
+            print(f"No lambda value found for beta_coef: {beta_coef_rounded}")

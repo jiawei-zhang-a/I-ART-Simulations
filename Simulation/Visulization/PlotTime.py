785c2b0fa4289758adf2b7a03c5ad0a92811d022
jiawei-zhang-a <jz4721@nyu.edu>
2024-10-18 16:01:18 -0500

    MAINT

--- Simulation/Visulization/PlotTime.py
+++ Simulation/Visulization/PlotTime.py
@@ -2,40 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-from ReadData import read_npz_files, read_npz_files_time
-
-def plot_results(data, title, xsticks):
-    columns = ['beta', 'Imputer_Median', 'Imputer_PREP-RidgeReg',  'Imputer_PREP-GBM', 'Imputer_Oracle']
-
-    df = pd.DataFrame(data, columns=columns)
-
-    plt.figure(figsize=(10, 6))
-
-    colors = {'Median': 'blue', 'PREP-RidgeReg': 'red', 'PREP-GBM': 'green', 'Oracle':'purple'}
-    linestyles = {'Imputer': '-'}
-
-    for col in columns[1:]:
-        method = col.split('_')[1]
-        dataset = col.split('_')[0]
-        linestyle = linestyles[dataset]
-        plt.plot(df['beta'], df[col], marker='o', label=method, color=colors[method], linestyle=linestyle, linewidth=2.0)
-        
-    plt.xlabel(r'$\beta$',fontsize=30)
-    plt.ylabel('Rejection Rate',fontsize=30)
-    plt.grid()
-    # Setting y-axis ticks with custom intervals
-    y_ticks = [i/100.0 for i in range(25, 105, 25)]  # Starts from 0, ends at 1.05, with an interval of 0.05
-    y_ticks.append(0.05)
-    plt.yticks(y_ticks)
-    X_ticks = xsticks
-    plt.xticks(X_ticks)
-    plt.tick_params(axis='both', which='major', labelsize=25)
-
-    #plt.show()
-    if not os.path.exists("pic"):
-        os.makedirs("pic")
-
-    plt.savefig("pic/" + title + ".pdf", bbox_inches='tight')
+from ReadData import read_npz_files_time
 
 
 def plot(range, range_small, path,path_small, title, title_small, multiple = False):
@@ -66,10 +33,10 @@ def plot(range, range_small, path,path_small, title, title_small, multiple = Fal
 
 def main_pic_generator():
 
-    plot(np.arange(0.0,0.42,0.07), np.arange(0,1.5,0.25), "../Power/timeL/HPC_power_1000_model1", "../Power/timeL/HPC_power_50_model1", "Size1000_Model1", "Size50_Model1")
-    plot(np.arange(0.0,0.96,0.16), np.arange(0.0,4.8,0.8), "../Power/timeL/HPC_power_1000_model2", "../Power/timeL/HPC_power_50_model2", "Size1000_Model2", "Size50_Model2")
-    plot(np.arange(0.0,0.36,0.06), np.arange(0.0,1.5,0.25), "../Power/timeL/HPC_power_1000_model3", "../Power/timeL/HPC_power_50_model3", "Size1000_Model3", "Size50_Model3")
-    plot(np.arange(0.0,0.36,0.06), np.arange(0.0,1.5,0.25), "../Power/timeL/HPC_power_1000_model4", "../Power/timeL/HPC_power_50_model4", "Size1000_Model4", "Size50_Model4")  
+    plot(np.arange(0.0,0.42,0.07), np.arange(0,1.5,0.25), "/scratch/jz4721/Post-prediction-Causal-Inference/Simulation/Power/timeL/HPC_power_1000_model1", "/scratch/jz4721/Post-prediction-Causal-Inference/Simulation/Power/timeL/HPC_power_50_model1", "Size1000_Model1", "Size50_Model1")
+    plot(np.arange(0.0,0.96,0.16), np.arange(0.0,4.8,0.8), "/scratch/jz4721/Post-prediction-Causal-Inference/Simulation/Power/timeL/HPC_power_1000_model2", "/scratch/jz4721/Post-prediction-Causal-Inference/Simulation/Power/timeL/HPC_power_50_model2", "Size1000_Model2", "Size50_Model2")
+    plot(np.arange(0.0,0.36,0.06), np.arange(0.0,1.5,0.25), "/scratch/jz4721/Post-prediction-Causal-Inference/Simulation/Power/timeL/HPC_power_1000_model3", "/scratch/jz4721/Post-prediction-Causal-Inference/Simulation/Power/timeL/HPC_power_50_model3", "Size1000_Model3", "Size50_Model3")
+    plot(np.arange(0.0,0.36,0.06), np.arange(0.0,1.5,0.25), "/scratch/jz4721/Post-prediction-Causal-Inference/Simulation/Power/timeL/HPC_power_1000_model4", "/scratch/jz4721/Post-prediction-Causal-Inference/Simulation/Power/timeL/HPC_power_50_model4", "Size1000_Model4", "Size50_Model4")  
     #plot(np.arange(0.2, 0.3, 0.05), np.arange(1.5, 2.5, 0.5), "../Data/Result/HPC_power_1000_Model5", "../Data/Result/HPC_power_50_Model5", "Size1000_Model5", "Size50_Model5", multiple=True)
 
 

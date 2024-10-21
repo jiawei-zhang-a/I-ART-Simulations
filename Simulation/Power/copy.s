1b59075880b3719d8a2ec142baf1fd055c3f6476
jiawei-zhang-a <jz4721@nyu.edu>
2024-10-20 21:18:11 -0500

    MAINT

---
+++ Simulation/Power/copy.s
@@ -0,0 +1,2 @@
+rsync -av --include="*/" --include="p_values_median_*.npy" --exclude="*" ../Data/Simulation/HPC_power_1000_model4/ Result/HPC_power_1000_model7/
+rsync -av --include="*/" --include="p_values_median_*.npy" --exclude="*" ../Data/Simulation/HPC_power_50_model4/ Result/HPC_power_50_model7/
--- Simulation/Visulization/ReadData.py
+++ Simulation/Visulization/ReadData.py
@@ -87,7 +87,7 @@ def read_npz_files(directory,small_size=False, multiple=False, type="original"):
         rejection_rate_oracle = -1
 
     results = {
-        'median_power': 0,
+        'median_power': rejection_rate_median,
         'lr_power': rejection_rate_LR,
         'xgboost_power':    rejection_rate_xgboost,
         'oracle_power': rejection_rate_oracle,

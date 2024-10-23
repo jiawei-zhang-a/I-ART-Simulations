b04b087558be97c4f77b4eafffd4082cdc60d388
jiawei-zhang-a <jz4721@nyu.edu>
2024-10-23 00:18:47 -0500

    MAINT

--- Simulation/Power/PlotL.s
+++ Simulation/Power/PlotL.s
@@ -13,7 +13,6 @@ export OMP_NUM_THREADS=1
 module purge
 
 singularity exec --overlay /scratch/jz4721/pyenv/overlay-15GB-500K.ext3:ro \
-    /scratch/work/publ
-    ic/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif \
+    /scratch/work/public/singularity/cuda11.6.124-cudnn8.4.0.27-devel-ubuntu20.04.4.sif \
     /bin/bash -c "source /ext3/env.sh; cd /scratch/jz4721/Post-prediction-Causal-Inference/Simulation/Visulization/; python3 /scratch/jz4721/Post-prediction-Causal-Inference/Simulation/Visulization/PlotLConvergence.py"
 

2335d4be8d78eeca53c17dbbf6088dac85c8dbc1
jiawei-zhang-a <jz4721@nyu.edu>
2024-10-30 13:39:28 -0500

    MAINT

--- Simulation/Power/copy.s
+++ Simulation/Power/copy.s
@@ -1,2 +1,12 @@
-rsync -av --include="*/" --include="p_values_median_*.npy" --exclude="*" ../Data/Simulation/HPC_power_1000_model4/ Result/HPC_power_1000_model7/
-rsync -av --include="*/" --include="p_values_median_*.npy" --exclude="*" ../Data/Simulation/HPC_power_50_model4/ Result/HPC_power_50_model7/
+rsync -av --include="*/" --include="p_values_median_*.npy" --exclude="*" ../Data/Simulation/HPC_power_1000_model4/ Result/HPC_power_1000_model4/
+rsync -av --include="*/" --include="p_values_median_*.npy" --exclude="*" ../Data/Simulation/HPC_power_50_model4/ Result/HPC_power_50_model4/
+
+rsync -av --include="*/" --include="p_values_median_*.npy" --exclude="*" ../Data/Simulation/HPC_power_1000_model3/ Result/HPC_power_1000_model3/
+rsync -av --include="*/" --include="p_values_median_*.npy" --exclude="*" ../Data/Simulation/HPC_power_50_model3/ Result/HPC_power_50_model3/
+
+rsync -av --include="*/" --include="p_values_median_*.npy" --exclude="*" ../Data/Simulation/HPC_power_1000_model2/ Result/HPC_power_1000_model2/
+rsync -av --include="*/" --include="p_values_median_*.npy" --exclude="*" ../Data/Simulation/HPC_power_50_model2/ Result/HPC_power_50_model2/
+
+rsync -av --include="*/" --include="p_values_median_*.npy" --exclude="*" ../Data/Simulation/HPC_power_1000_model1/ Result/HPC_power_1000_model1/
+rsync -av --include="*/" --include="p_values_median_*.npy" --exclude="*" ../Data/Simulation/HPC_power_50_model1/ Result/HPC_power_50_model1/
+
